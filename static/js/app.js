5978aad5bd3fc8ac912c20ae8e30ea887d28c52e
unknown <crkotowski@gmail.com>
2021-05-23 18:27:21 -0500

    Created table to hold UFO data

--- static/js/app.js
+++ static/js/app.js
@@ -1,2 +1,25 @@
 // import the data from data.js
 const tableData = data;
+// Reference the HTML table using d3
+var tbody = d3.select("tbody")
+
+// Building our UFO table
+function buildTable(data) {
+    // clear any existing data
+    tbody.html("");
+
+    //Loop through each object in data
+    //Append a row and cell for eachv alue in row
+    data.forEach((dataRow) => {
+        // append a  row to table body
+        let row = tbody.append("tr");
+
+        // Loop through each field in dataRow and
+        // add each falvue as table cell (td)
+        Object.values(dataRow).forEach((val) => {
+          let cell = row.append("td");
+          cell.text(val);
+          }
+        );
+      });
+}
